4ed4743b7c9fda404978acb8960a194e196785dd
imkattyy <katush.kovaleva2015@mail.ru>
2023-09-08 23:05:01 +0300

    1

--- src/mirea/ivbo1/Task5.java
+++ src/mirea/ivbo1/Task5.java
@@ -1,7 +1,29 @@
 package mirea.ivbo1;
+import java.util.Scanner;
 
 public class Task5 {
     public static void main(String[] args) {
+        System.out.println("Введите число, факториал которого хотите вычислить: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        System.out.println("Факториал " + number + " равен " + calculateFactorial(number));
+        scanner.close();
 
     }
+
+    public static long calculateFactorial(int n) {
+        if (n < 0)
+            throw new IllegalArgumentException("Факториал отрицательного числа не определен.");
+
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
 }
+
+
